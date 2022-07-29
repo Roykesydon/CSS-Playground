35517e3baec7beb7730ae86d5ca9715b332cdb4b
Roykesydon <roy23381547@gmail.com>
2022-07-29 18:25:32 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row class="pa-10" justify="center" align="center">
+    <v-col
+      v-for="(effect, i) in effects"
+      :key="i"
+      cols="3"
+      class="mt-10"
+      justify="center"
+      align="center"
+    >
+      <v-img
+        @click="changeRoute(effect.route)"
+        class="white--text align-end pointer"
+        height="30vh"
+        width="30vh"
+        :src="effect.src"
+      >
+        <v-card-title class="font-weight-light">{{ effect.name }}</v-card-title>
+      </v-img>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    effects: [
+      {
+        name: "Glitch",
+        route: "/glitch",
+        src: require("@/assets/cover/glitch.png"),
+      },
+    ],
+  }),
+  methods: {
+    changeRoute(route) {
+      this.$router.push(route);
+    },
+  },
+};
+</script>
+
+<style scope>
+.pointer {
+  cursor: pointer;
+}
+</style>
